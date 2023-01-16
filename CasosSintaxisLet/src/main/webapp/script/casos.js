e38a5bfa933783898ccbb36c04a857da24424e33
mnocea <mnoceacabrera@gmail.com>
2023-01-16 12:59:21 +0100

    Primer Commit

---
+++ CasosSintaxisLet/src/main/webapp/script/casos.js
@@ -0,0 +1,35 @@
+function caso1(){
+	var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);//a=5
+  
+
+document.write(b);//b=1
+}
+
+document.write(a);//a=5
+document.write(b);//b=1
+}
+function caso2(){
+	for (let i = 0; i<10; i++) {
+document.write(i);
+}
+document.write(i);
+}
+function caso3(){
+	var x = 5;
+var y = 0;
+
+document.write(x + y);
+}
+function caso4(){
+	var a = 5;
+document.write(a);
+}
